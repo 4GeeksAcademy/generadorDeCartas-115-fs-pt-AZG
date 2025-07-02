c35a5cedef74be0db95a7329fa24bd36746af543
albertoZG <albertozg94@gmail.com>
2025-07-02 19:21:19 +0000

    done6

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const palos = [
+    { nombre: 'spade', simbolo: '♠' },
+    { nombre: 'club', simbolo: '♣' },
+    { nombre: 'heart', simbolo: '♥' },
+    { nombre: 'diamond', simbolo: '♦' }
+  ];
+  const valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  let cartasHTML = ''
+
+
+  
+    const randomPalo = palos[Math.floor(Math.random() * palos.length)];
+    const randomValor = valores[Math.floor(Math.random() * valores.length)];
+
+    cartasHTML = ` 
+ <div class="card ${randomPalo.nombre}">
+      <div class="top-left">${randomValor}</div>
+      <div class="center">${randomPalo.simbolo}</div>
+       <div class="bottom-right">${randomValor}</div>
+</div>
+`;
+
+  
+
+  document.getElementById("carta").innerHTML = cartasHTML;
+}
+
+// window.onload = generarCarta;
+// document.getElementById("generarCarta").addEventListener("click",generarCarta);
